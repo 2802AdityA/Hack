7cfa3deac466ef5aea80426cbcf72423ab77f384
muskan garg <muskangarg02270@gmail.com>
2023-04-02 04:41:10 +0530

    blog

--- src/components/Blog/BlogPost.jsx
+++ src/components/Blog/BlogPost.jsx
@@ -1,31 +1,44 @@
 import '../../styles/components/BlogCard.css';
 // import blogPosts from '../../assets/dummydata';
 import { useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_POST = gql`
+  query getPost($id: uuid!) {
+    document(id: $id) {
+      id
+      title
+      author_name
+      date
+      content
+    }
+  }
+`;
+
 
 const BlogPost = () => {
 
 
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_POST, {
+    variables: { id },
+  });
 
-  //   const { id } = useParams();
-  //   console.log(id);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
 
-  //   const blog = blogPosts.find(post => post.id === Number(id));
+  const blogPost = data.document;
 
-  //   console.log(blog);
+  return (
+    <div className="blog-post">
+      <h1>{blogPost.title}</h1>
+      <p className="date"> <a href={`/userBlog/${blogPost.author_name}`}><em>@{blogPost.author_name}</em> </a></p>
+      <p className="date">{blogPost.date}</p>
+      <p className="text">{blogPost.content} </p>
+    </div>
+  );
 
-  //   return (
-  //     <div className="blog-post">
-  //       <div className="content">
-  //         <div className="card"><h2>{blog.title}</h2>
-  //           <div className="info">
-  //             <p className="date"> <a href={`/userBlog/${blog.author}`}><em>{blog.author}</em> </a></p>
-  //             <p className="date">{blog.date}</p>
-  //           </div>
-  //           <p className="text"> {blog.excerpt}</p>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // };
+};
+
 
   export default BlogPost;
