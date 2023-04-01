ca0038a51da7e6659cbce97131889c52ae827109
Mahikagarg09 <mahika_garg@yahoo.com>
2023-04-01 22:13:28 +0530

    text color change

--- src/pages/Exercise_blog.js
+++ src/pages/Exercise_blog.js
@@ -4,13 +4,13 @@ import data from ".././components/Exercise"
 export default function Exercise_blog() {
     return (
         <div className="p-4 space-y-16">
-            <p className="text-7xl font-serif font-semibold text-teal-800 mx-auto text-center">Exercises</p>
-            <p className="text-3xl font-serif text-center text-green-900">Breathe In, Stress Out: Exercises for a Calm and Clear Mind</p>
+            <p className="text-7xl font-serif font-semibold text-[#5d001e] mx-auto text-center">Exercises</p>
+            <p className="text-3xl font-serif text-center text-[#9a1750]">Breathe In, Stress Out: Exercises for a Calm and Clear Mind</p>
             {data.map((item) => {
                 return (
                     <div key={item.key} className="p-4 space-y-8">
-                        <p className="text-5xl font-serif font-semibold text-teal-800">{item.key}.{item.name}</p>
-                        <p className="text-xl">{item.description}</p>
+                        <p className="text-5xl font-serif font-semibold text-[#ee4c7c]">{item.key}.{item.name}</p>
+                        <p className="text-xl ">{item.description}</p>
                         <iframe
                             width="100%"
                             height="450"
