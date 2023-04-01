ef66e4454c9292c1a175b87e0daac2d5d7f021e8
Mahikagarg09 <mahika_garg@yahoo.com>
2023-04-02 01:01:51 +0530

    edit navbar - add links

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -54,13 +54,14 @@ const Layout = () => {
 
   return (
     <div>
-      <header className={styles.header}>
-        <div className={styles['header-container']}>
-          <Link to="/">
-            <img src={process.env.PUBLIC_URL + 'logo.svg'} alt="logo" />
+      <nav class="bg-[#5d001e]  fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
+          <Link to="/" class="flex items-center">
+            <img src={process.env.PUBLIC_URL + 'logo.png'} class="h-20" alt="Flowbite Logo"/>
           </Link>
-
-          <Menu as="div" className={styles.menu}>
+          <div class="flex md:order-2">
+            <button type="button" class="text-white focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0">
+            <Menu as="div" className={styles.menu}>
             <Menu.Button className={styles['menu-button']}>
               <Avatar src={user?.avatarUrl} alt={user?.displayName} />
               <ChevronDownIcon />
@@ -105,8 +106,31 @@ const Layout = () => {
               </Menu.Items>
             </Transition>
           </Menu>
+            </button>
+            <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+              <span class="sr-only">Open main menu</span>
+              <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            </button>
+          </div>
+          <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium text-xl border  rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-[#5d001e]">
+            <li>
+                <Link to="/"  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Chat Bot</Link>
+              </li>
+              <li>
+                <Link to="/"  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Exercises</Link>
+              </li>
+              <li>
+                <Link to="/"  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blog</Link>
+              </li>
+              <li>
+                <Link to="/"  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Peer Chat</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </header>
+      </nav>
+
 
       <main className={styles.main}>
         <div className={styles['main-container']}>
