afe3d5c0fde7246941b5f981e55dde81d91bb520
Mahikagarg09 <mahika_garg@yahoo.com>
2023-04-02 07:09:13 +0530

    position of form

--- src/pages/Videocall/Home.js
+++ src/pages/Videocall/Home.js
@@ -18,15 +18,15 @@ export default function Home() {
     <div className="">
       <div className="relative">
         <div className=" absolutemax-h-[30rem] flex overflow-hidden ">
-          <img src="https://images.unsplash.com/photo-1502444330042-d1a1ddf9bb5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y29ubmVjdGluZyUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+          {/* <img src="https://images.unsplash.com/photo-1502444330042-d1a1ddf9bb5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y29ubmVjdGluZyUyMHBlb3BsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
             className="object-cover w-full h-screen  z-0"
             alt="random"
-          />
+          /> */}
         </div>
         <div className="absolute top-0 left-0 w-full  flex items-center justify-center">
         <form action="" onSubmit={submitCode}
-          className="flex flex-col items-center justify-center">
-          <label htmlFor="" className="font-bold text-[50px] pt-12 text-white">Enter The Room Code</label>
+          className="flex flex-col items-center justify-center mt-60">
+          <label htmlFor="" className="font-bold text-[50px] pt-12 text-black">Enter The Room Code</label>
           <input
             type="text"
             required
