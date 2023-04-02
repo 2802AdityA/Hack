45b117f8328b1fc2748ee2bb7ab3d74e5e005dde
Mahikagarg09 <mahika_garg@yahoo.com>
2023-04-02 08:25:35 +0530

    therapist page

---
+++ src/pages/Drconnect.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Drconnect() {
+  return (
+    <div style={{marginTop:"100px"}}>
+      <p className="text-3xl font-serif font-semibold text-[#5d001e] mx-auto text-center">Get appointment now!</p>
+      <p className="2xl"> COMING SOON</p>
+    </div>
+  );
+}
