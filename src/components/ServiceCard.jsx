871944b8ab3524a0ca0ac4f3612eb93bc6a6118b
Mahikagarg09 <mahika_garg@yahoo.com>
2023-04-02 07:03:53 +0530

    setup routes

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import '../styles/components/ServiceCard.css';
+import { useNavigate } from 'react-router-dom';
 
-const ServiceCard = ({type, name}) => {
+const ServiceCard = ({type, name,route}) => {
+
+  let navigate = useNavigate();
+
+  const handleCardClick = () => {
+    console.log("route: ", route);
+    navigate(route);
+  };
+  
+
   return (
-    <div className="service-card">
+    <div className="service-card" onClick={ handleCardClick} >
         <div className="service-card-img">
             <div className="service-card-title">
                 <h1>{name}</h1>
